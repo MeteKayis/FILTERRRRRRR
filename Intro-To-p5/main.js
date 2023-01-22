ff993f1ea640cbff889be8e18dfe6d170271e3cb
MeteKayis <102928455+MeteKayis@users.noreply.github.com>
2023-01-22 18:40:13 -0500

    Add files via upload

---
+++ Intro-To-p5/main.js
@@ -0,0 +1,30 @@
+function preload()
+{
+
+}
+
+function setup() {
+    canvas = createCanvas(600,600);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color = "";
+}
+
+function draw()
+{
+    image(video, 0 ,0, 600, 600);
+    tint(tint_color);
+}
+
+function take_snapshot()
+{
+    save('mete.png');
+}
+
+function filter_tint()
+{
+    tint_color = document.getElementById("color_name").value;
+}
+
